fb922a1baaefd3b497e73b3474a1df1b0d352dda
saideeprai965 <saideeprai965@gmail.com>
2026-01-27 19:28:37 +0530

    Add Biot-Savart law calculation and plotting

---
+++ src/biotsavart.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+mu_0 = 4 * np.pi * 1e-7   
+I = 5.0                  
+
+
+r = np.linspace(0.01, 0.1, 100)
+
+B = (mu_0 * I) / (2 * np.pi * r)
+
+# Plot
+plt.plot(r, B)
+plt.xlabel("Distance (m)")
+plt.ylabel("Magnetic Field (T)")
+plt.title("Magnetic Field vs Distance (Biot-Savart Law)")
+plt.grid(True)
+
+plt.savefig("biot_savart_field.png", dpi=300)
+plt.show()
