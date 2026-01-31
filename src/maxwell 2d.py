cd51c38f203146a2191d942db1c89ff2de0c2bda
saideeprai965 <saideeprai965@gmail.com>
2026-01-31 14:11:59 +0530

    Refactor electric field calculation and update plot

--- src/maxwell 2d.py
+++ src/maxwell 2d.py
@@ -1,20 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+epsilon0 = 8.854e-12
+Q = 1e-6
+
 x = np.linspace(-1, 1, 20)
 y = np.linspace(-1, 1, 20)
 X, Y = np.meshgrid(x, y)
 
 
-k = 5
-Ex = k * X
-Ey = np.zeros_like(Y)
+r = np.sqrt(X*2 + Y*2) + 1e-9
+
+
+Ex = Q * X / (4 * np.pi * epsilon0 * r**3)
+Ey = Q * Y / (4 * np.pi * epsilon0 * r**3)
 
 plt.figure(figsize=(6, 6))
 plt.quiver(X, Y, Ex, Ey)
-plt.xlabel("x (m)")
-plt.ylabel("y (m)")
-plt.title("Electric Field Vector Plot (Linear Field)")
+plt.plot(x, x, 'k')     
+plt.plot(x, -x, 'k')    
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Electric Field Vector Plot (2D)")
 plt.grid(True)
 plt.axis("equal")
 
